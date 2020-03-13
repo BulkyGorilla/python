9f5feca1ac97a1ba130c8646a4a2a6acf930fb44
BulkyGorilla <isar4@icloud.com>
2020-03-13 23:09:25 +0700

    Create scratch file
    เริ่มลองสร้างไฟล์ครั้งแรก

---
+++ scratch.py
@@ -0,0 +1,13 @@
+stock_list = []
+
+print(stock_list)
+
+number_added = int(input("How many item you want to add? : "))
+
+for add in range(number_added):
+    name_added = str(input("Item name : "))
+    stock_list.append(name_added)
+
+print("Item in stock is")
+for show in stock_list:
+    print(,show)
